e0e8ab166960c26ac598ffa92b3ca5132b6cd6e8
liaoqianwen <liaoqianwen89@gmail.com>
2017-01-03 16:44:52 +0800

    update login page

--- doubanfilm/pages/index/index.js
+++ doubanfilm/pages/index/index.js
@@ -8,9 +8,26 @@ Page({
   },
   //事件处理函数
   bindViewTap: function() {
-    wx.navigateTo({
-      url: '../douban/in_theathers/in_theathers'
+    // 跳转应用内非tabBar页面
+    // wx.navigateTo({
+    //   //url: '../douban/in_theathers/in_theathers' 需要跳转的应用内非 tabBar       //的页面的路径 , 路径后可以带参数
+    //   url: '../douban/detail/detail'
+    // })
+    
+    //跳转应用内tabBar页面
+    wx.switchTab({
+      url: '../douban/in_theathers/in_theathers',
+      success: function(res){
+        // success
+      },
+      fail: function() {
+        // fail
+      },
+      complete: function() {
+        // complete
+      }
     })
+
   },
   onLoad: function () {
     console.log('onLoad')
